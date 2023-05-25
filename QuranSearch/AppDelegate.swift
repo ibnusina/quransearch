b0b82994cfaffee6bd81edb716de1e88fbdb3ce5
ibnu <ibnu.sina009@gmail.com>
2023-05-25 16:14:49 +0700

    add codemagic and firebase

--- QuranSearch/AppDelegate.swift
+++ QuranSearch/AppDelegate.swift
@@ -8,15 +8,40 @@
 import UIKit
 import UIKit
 import RealmSwift
+import FirebaseCore
+import FirebaseRemoteConfig
 
 let localRealm = try! Realm(configuration: Realm.Configuration(readOnly: false, schemaVersion: 5, migrationBlock: { migration, oldSchemaVersion in }, deleteRealmIfMigrationNeeded: true))
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var remoteConfig: RemoteConfig!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+        remoteConfig = RemoteConfig.remoteConfig()
+        let settings = RemoteConfigSettings()
+        settings.minimumFetchInterval = 0
+        remoteConfig.configSettings = settings
+        remoteConfig.setDefaults(fromPlist: "remote_config_defaults")
+        
+        print("masup \(remoteConfig.configValue(forKey: "dummy_test_2").stringValue)")
+        
+        // [START fetch_config_with_callback]
+            remoteConfig.fetch { (status, error) -> Void in
+              if status == .success {
+                print("Config fetched!")
+                self.remoteConfig.activate { changed, error in
+                    print("masup 1 s\(changed)")
+                }
+              } else {
+                print("Config not fetched")
+                print("Error: \(error?.localizedDescription ?? "No error available.")")
+              }
+                print("masup 3 \(self.remoteConfig.configValue(forKey: "dummy_test_2").stringValue)")
+            }
+        
         if localRealm.objects(Chapter.self).count <= 0 {
             loadLanguage()
         }
