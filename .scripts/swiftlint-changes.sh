a0d11dbeae12aadebf7012ddc214ed090f05f60c
ibnu <ibnu.sina009@gmail.com>
2023-07-28 15:01:21 +0700

    test

--- .scripts/swiftlint-changes.sh
+++ .scripts/swiftlint-changes.sh
@@ -4,7 +4,7 @@ function convertToGitHubActionsLoggingCommands() {
     sed -E 's/^(.*):([0-9]+):([0-9]+): (error|[^:]+): (.*)/::\4 file=\1,line=\2,col=\3::\5/'
 }
 
-changedFiles=$(git --no-pager diff --name-only --relative $1 $(git merge-base $1 $2) -- '*.swift' | grep -Ev 'Test|test|Pods')
+changedFiles=$(git --no-pager diff --name-only --relative $1 $(git merge-base $1 $2) -- '*.swift' | grep -Ev 'Test/|test/|Tests/|tests/|Pods/')
 
 if [ -z "$changedFiles" ]
 then
